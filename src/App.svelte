e4e1f3c912c8147ba1850dc3fc78916e44f5773d
Squidtoon99 <squidtoon99@gmail.com>
2021-12-11 23:13:44 -0600

    full push

---
+++ src/App.svelte
@@ -0,0 +1,174 @@
+<script>
+	import AutoComplete from "simple-svelte-autocomplete";
+	import Song from "./Song.svelte";
+	let genBtnDisabled = true;
+	let currentSong = {};
+	$: {
+		genBtnDisabled = currentSong.name == undefined;
+	}
+
+	var results = [];
+
+	const API_URL = "https://api.spotify.com/v1/";
+	const accessToken =
+		"BQDQu7JY_xi8yt9uQbpgQIeAYDhDmwyazB8LuPJhMzyjBD4EQsof7APbTXQtx2rb-DtwO8LUIoA8JhQFqjY";
+
+	const findSongs = async (query) => {
+		var url = new URL(API_URL + "search?");
+		var params = new URLSearchParams({
+			q: query,
+			type: "track",
+			limit: 5,
+		});
+
+		const res = await fetch(url + params, {
+			headers: {
+				Authorization: "Bearer " + accessToken,
+			},
+		});
+		console.log(res);
+		if (res.ok) {
+			const data = await res.json();
+			console.log(data);
+			return data.tracks.items.map((track) => {
+				let pretty = `${track.name} - ${track.artists
+					.map((artist) => artist.name)
+					.join(", ")}`;
+				if (pretty.length >= 35) {
+					pretty = pretty.substring(0, 35) + "...";
+				}
+				return {
+					id: track.id,
+					name: track.name,
+					artists: track.artists
+						.map((artist) => artist.name)
+						.join(", "),
+					album: track.album.name,
+					image: track.album.images[0].url,
+					preview: track.preview_url,
+					uri: track.uri,
+					pretty: pretty,
+				};
+			});
+		} else {
+			return [];
+		}
+	};
+
+	const generateSongs = async () => {
+		results = [];
+
+		const url = new URL(API_URL + "recommendations?");
+		var params = new URLSearchParams({
+			seed_tracks: currentSong.id,
+			limit: 8,
+		});
+
+		const res = await fetch(url + params, {
+			headers: {
+				Authorization: "Bearer " + accessToken,
+			},
+		});
+		console.log(res);
+		currentSong = {};
+		genBtnDisabled = false;
+		document.getElementById("gen-btn").classList.remove("is-loading");
+		if (res.ok) {
+			const data = await res.json();
+			console.log(data);
+			results = data.tracks.map((track) => {
+				let pretty = `${track.name} - ${track.artists
+					.map((artist) => artist.name)
+					.join(", ")}`;
+				if (pretty.length >= 35) {
+					pretty = pretty.substring(0, 35) + "...";
+				}
+				return {
+					id: track.id,
+					name: track.name,
+					artists: track.artists
+						.map((artist) => artist.name)
+						.join(", "),
+					album: track.album.name,
+					image: track.album.images[0].url,
+					preview: track.preview_url,
+					uri: track.uri,
+					pretty: pretty,
+				};
+			});
+		}
+	};
+</script>
+
+<section class="section">
+	<div class="container">
+		<div class="columns is-centered">
+			<h1 class="title is-2 has-text-white">Song Recommender</h1>
+		</div>
+	</div>
+
+	<div class="container is-fluid">
+		<div id="song-input" class="section">
+			<AutoComplete
+				placeholder="Search for a song"
+				class="input"
+				searchFunction={findSongs}
+				delay="500"
+				localFiltering="false"
+				labelFieldName="pretty"
+				valueFieldName="id"
+				bind:selectedItem={currentSong}
+			/>
+
+			<button
+				id="gen-btn"
+				on:click={(e) => {
+					e.target.classList.add("is-loading");
+					genBtnDisabled = true;
+
+					return generateSongs();
+				}}
+				class="button mt-2 w-auto is-fullwidth is-primary"
+				disabled={genBtnDisabled}
+				style={`background: -webkit-linear-gradient(45deg, #8a86db, ${
+					genBtnDisabled ? "red" : "#00ff95"
+				} 80%);`}
+			>
+				<span class="icon is-small">
+					<i class="fas fa-search" />
+				</span>
+				<span> Generate </span>
+			</button>
+		</div>
+	</div>
+	{#if results.length > 0}
+		{#each results as song, i}
+			<div class="container">
+				<Song
+					id={song.id}
+					name={song.name}
+					artists={song.artists}
+					album={song.image}
+					delay={i * 300}
+				/>
+			</div>
+		{/each}
+	{/if}
+</section>
+
+<style>
+	:global(body) {
+		background: #181818;
+		color: #ffffff;
+	}
+	:global(section) {
+		background: #181818;
+		color: #ffffff;
+	}
+
+	#gen-btn {
+		background: -webkit-linear-gradient(45deg, #8a86db, red 80%);
+		border: 0;
+		color: white;
+	}
+</style>
---
+++ src/Song.svelte
@@ -0,0 +1,76 @@
+<script>
+    import { fade, fly } from "svelte/transition";
+
+    export let name;
+    export let artists;
+    export let id;
+    export let delay;
+    let visible = false;
+    setTimeout(() => {
+        visible = true;
+    }, delay);
+
+    let winHeight;
+</script>
+
+<svelte:window bind:innerHeight={winHeight} />
+{#if visible}
+    <div {id} class="song" in:fly={{ y: winHeight, duration: 1500 }} out:fade>
+        <div class="container" id="song">
+            <div class="columns">
+                <div class="column is-half">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+                                    <figure class="image is-96x96">
+                                        <!-- <img src={album} alt="Placeholder" /> -->
+                                        <iframe
+                                            title={name}
+                                            src={`https://open.spotify.com/embed/track/${id}`}
+                                            width="85%"
+                                            height="85%"
+                                            frameborder="0"
+                                            allowtransparency="true"
+                                            allow="encrypted-media"
+                                        />
+                                    </figure>
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4 has-text-weight-bold">
+                                        {name}
+                                    </p>
+                                    <p class="subtitle is-6">
+                                        by <strong>{artists}</strong>
+                                    </p>
+                                </div>
+                            </div>
+
+                            <!-- <div class="content is-hidden-mobile">
+                                <iframe
+                                    title={name}
+                                    src={`https://open.spotify.com/embed/track/${id}`}
+                                    width="100%"
+                                    height="100"
+                                    frameborder="0"
+                                    allowtransparency="true"
+                                    allow="encrypted-media"
+                                />
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .song {
+        transition: all 0.2s ease-in-out;
+    }
+
+    .song:hover {
+        transform: scale(1.01);
+    }
+</style>
